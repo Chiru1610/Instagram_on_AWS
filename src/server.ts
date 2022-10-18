2243b2e2ca4b156e12c7ab4b0310796b5bd34700
Chiranjeevi RudregowdaChiranjeevi <chiranjeevi.rudregowda@philips.com>
2022-10-18 09:44:06 +0530

    updating the latest code

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import {filterImageFromURL, deleteLocalFiles, isValidImage, isValidUrl} from './util/util';
 
 (async () => {
 
@@ -13,43 +13,42 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
-  //! END @TODO1
   
 
   // RESTFUL Endpoint for FilterObjects which takes image_url in queryParams
   app.get('/filteredimage',async (req,res) => {
 
     const {image_url} = req.query;
-
+    
     if(image_url === undefined || !image_url ){
-      res.status(400)
-          .send('image_url is required !!!.');
+      res.status(404)
+          .send('URL for the image was not found !!!.');
     }
 
+    if (!isValidUrl(image_url)) {
+      return res.status(400).send({ error: 'image_url is invalid' })
+    }
+
+    if (!isValidImage(image_url)) {
+      return res
+        .status(422)
+        .send({ error: 'image_url is not a valid image' })
+    }
+
+    try{
     const filterImageresult = await filterImageFromURL(image_url);
-    
     res.status(200)
         .sendFile(filterImageresult,{},async function(err){
           if(!err){
             await deleteLocalFiles([filterImageresult]);
           }
     });
+    }catch (error) {
+          return res
+            .status(422)
+            .send({ error: 'error image could not be processed' })
+    }
+    
    
   });
 
