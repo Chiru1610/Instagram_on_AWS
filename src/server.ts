ca44e070e8e28127cab2fc2d9e7fa9812d7e2676
Chiranjeevi RudregowdaChiranjeevi <chiranjeevi.rudregowda@philips.com>
2022-10-31 17:35:25 +0530

    dummy chnage

--- src/server.ts
+++ src/server.ts
@@ -36,7 +36,10 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
-  
+
+
+
+
 
   // Start the Server
   app.listen( port, () => {
